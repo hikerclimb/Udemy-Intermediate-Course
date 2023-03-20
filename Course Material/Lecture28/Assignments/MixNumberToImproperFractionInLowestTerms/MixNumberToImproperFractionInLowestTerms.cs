f977c1227528b53ca6c007e7a48ff8bd77dfecdb
Aniket Karmarkar <akarmarkar@phillipcapital.com>
2023-03-20 10:04:37 -0500

    Mixed Number in lowest terms

--- Course Material/Lecture28/Assignments/MixNumberToImproperFractionInLowestTerms/MixNumberToImproperFractionInLowestTerms.cs
+++ Course Material/Lecture28/Assignments/MixNumberToImproperFractionInLowestTerms/MixNumberToImproperFractionInLowestTerms.cs
@@ -13,17 +13,48 @@ namespace MixNumberToImproperFractionInLowestTerms
             int B; 
             int C;
 
-            /*A = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter A"));
+            A = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter A"));
             B = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter B"));
-            C = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter C"));*/
+            C = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter C"));
 
             int number = A * C + B;
-
-            double outp = 10.2 + .333;
-            decimal outp1 = 10.2M + 1/3;
-
-            MessageBox.Show(outp + ":" + outp1);
+            int gcdName = gcd(number, C );
+            int numGcd = number / gcdName;
+            int divGcd = C / gcdName;
+            MessageBox.Show(number + "/" + divGcd);
             
+        }
+
+        private int gcd(int m, int n)
+        {
+            int temp;
+            int r;
+
+            if (m < n)
+            {
+                temp = m;
+                m = n;
+                n = temp;
+            }
+
+            while (true)
+            {
+                r = m % n;
+                if (r == 0)
+                    break;
+
+                m = n;
+                n = r;
+            }
+
+            return n;
+        }
+
+        private int lcd(int x, int y)
+        {
+            //the lcd of two numbers x and y is x*y/gcd(x,y)
+
+            return x * y / gcd(x, y);
         }
     }
 }
