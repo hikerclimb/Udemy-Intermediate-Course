587a4f310557f9b3e4f04dfb833f4946a4d5bd04
Aniket Karmarkar <akarmarkar@phillipcapital.com>
2023-03-20 13:18:03 -0500

    added 2 fractions together

---
+++ Course Material/Lecture28/Assignments/Add2FractionsAndPutInLowestTerms/Add2FractionsAndPutInLowestTerm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Add2FractionsAndPutInLowestTerms
+{
+    partial class Add2FractionsAndPutInLowestTerm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            BtnConvert = new Button();
+            SuspendLayout();
+            // 
+            // BtnConvert
+            // 
+            BtnConvert.Location = new Point(363, 92);
+            BtnConvert.Name = "BtnConvert";
+            BtnConvert.Size = new Size(75, 23);
+            BtnConvert.TabIndex = 0;
+            BtnConvert.Text = "Convert";
+            BtnConvert.UseVisualStyleBackColor = true;
+            BtnConvert.Click += BtnConvert_Click;
+            // 
+            // Add2FractionsAndPutInLowestTerm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(BtnConvert);
+            Name = "Add2FractionsAndPutInLowestTerm";
+            Text = "Add2FractionsAndPutInLowestTerm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button BtnConvert;
+    }
+}
---
+++ Course Material/Lecture28/Assignments/Add2FractionsAndPutInLowestTerms/Add2FractionsAndPutInLowestTerm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Add2FractionsAndPutInLowestTerms
+{
+    public partial class Add2FractionsAndPutInLowestTerm : Form
+    {
+        public Add2FractionsAndPutInLowestTerm()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnConvert_Click(object sender, EventArgs e)
+        {
+            int A;
+            int B;
+            int C;
+            int D;
+
+            A = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter A"));
+            B = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter B"));
+            C = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter C"));
+            D = Convert.ToInt32(Microsoft.VisualBasic.Interaction.InputBox("Enter D"));
+
+            int firstnumerator = D * A;
+            int secondnumerator = C*B;
+            int denominator = B * D;
+
+            int numeratorOf2 = firstnumerator + secondnumerator;
+
+            int gcdOut = gcd(numeratorOf2, denominator);
+
+            int denominatorOut = denominator / gcdOut;
+            int numeratorOut = numeratorOf2 / gcdOut;
+
+            MessageBox.Show(numeratorOut + "/" + denominatorOut);
+        }
+
+        private int gcd(int m, int n)
+        {
+            int temp;
+            int r;
+
+            if (m < n)
+            {
+                temp = m;
+                m = n;
+                n = temp;
+            }
+
+            while (true)
+            {
+                r = m % n;
+                if (r == 0)
+                    break;
+
+                m = n;
+                n = r;
+            }
+
+            return n;
+        }
+
+        private int lcd(int x, int y)
+        {
+            //the lcd of two numbers x and y is x*y/gcd(x,y)
+
+            return x * y / gcd(x, y);
+        }
+    }
+}
