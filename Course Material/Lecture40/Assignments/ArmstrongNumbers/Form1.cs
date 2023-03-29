50f7f1cf72ed312e1f1cea9f15a05ca28441f16b
Aniket Karmarkar <karmarkaraniket@gmail.com>
2023-03-29 13:54:02 -0500

    better way of doing armstrong numbers

--- Course Material/Lecture40/Assignments/ArmstrongNumbers/Form1.cs
+++ Course Material/Lecture40/Assignments/ArmstrongNumbers/Form1.cs
@@ -1,3 +1,5 @@
+using System.Drawing.Text;
+
 namespace ArmstrongNumbers
 {
     public partial class Form1 : Form
@@ -9,37 +11,24 @@ namespace ArmstrongNumbers
 
         private void BtnArmstrongNumbers_Click(object sender, EventArgs e)
         {
+            
             for (int i = 1; i <= 500; i++)
             {
-                int num1 = 0;
-                int num2 = 0;
-                int num3 = 0;
-                int num3rem = 0;
-                if (i.ToString().Length == 1)
-                {
-                    num1 = i;
-                }
-                if (i.ToString().Length == 2 )
-                {
-                    num2 = i % 10;
-                    num1 = (int)Math.Floor(i / 10.0);
-                }
-                if (i.ToString().Length == 3)
-                {
-                    num3 = i % 10;
-                    num3rem = i / 10;
-                    num2 = (num3rem) %10;
-                    num1 = i / 100;
-                }
-
-                var otu = num1 * num1 * num1 + num2 * num2 * num2 + num3 * num3 * num3;
-                if(i == otu)
-                {
-                    TxtOutpt.Text += "i == out" + i +": " +otu + Environment.NewLine;
-                }
+                ArmstrongNumbers(i);
             }
         }
 
-       
+        private void ArmstrongNumbers(int num)
+        {
+            int otu = 0;
+            int castBackToNumber = 0;
+            foreach (var item in num.ToString())
+            {
+                castBackToNumber = (int)Char.GetNumericValue(item);
+                otu += (int)Math.Pow(castBackToNumber, 3);
+                if (num == otu)
+                    TxtOutpt.Text += num + Environment.NewLine;
+            }
+        }
     }
 }
